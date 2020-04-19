6bec0adf1b2d85b99d44da113b6ae0fc9dd48294
Rupa Dhillon <rupa211984@gmail.com>
2020-04-18 19:38:56 -0700

    No budgets message

--- src/components/dashboard/Budgets.js
+++ src/components/dashboard/Budgets.js
@@ -2,8 +2,11 @@ import React from 'react';
 import Budget from './Budget';
 
 function Budgets({ budgets }) {
+  const noBudgets = () => budgets.length === 0 ? <li>You don't have any budgets</li> : '';
+
   return (
     <ul>
+      { noBudgets() }
       { budgets.map(budget => <li key={budget.id}><Budget budget={budget}/></li>) }
     </ul>
   )
