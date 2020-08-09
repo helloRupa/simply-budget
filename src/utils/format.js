a0cbde48c957a17496905f0867a5c7c611ee00fc
Rupa Dhillon <rupa211984@gmail.com>
2020-08-08 22:34:08 -0700

    Fix number formatter

--- src/utils/format.js
+++ src/utils/format.js
@@ -1,20 +1,5 @@
 export function formatNumber(value) {
-  if (typeof value === 'number') {
-    value = value.toString(10);
-  }
-
-  const dotIndex = value.indexOf('.');
-
-  switch (dotIndex) {
-    case -1:
-      return `${value}.00`;
-    case value.length - 1:
-      return `${value}00`;
-    case value.length - 2:
-      return `${value}0`;
-    default:
-      return value;
-  }
+  return parseFloat(value).toFixed(2);
 };
 
 // export function formatBudgets(budgets) {
