454860d05f8305a90c3860ff97006224f6d0d121
Rupa Dhillon <rupa211984@gmail.com>
2020-08-31 23:06:39 -0700

    Remove unused ui actions

--- src/actions/ui_actions.js
+++ src/actions/ui_actions.js
@@ -10,9 +10,3 @@ export const chooseBudget = () => changeView('BUDGET');
 export const chooseSettings = () => changeView('SETTINGS');
 
 export const chooseArchive = () => changeView('ARCHIVE');
-
-export const chooseCreateBudget = () => changeView('CREATE_BUDGET');
-
-export const chooseEditBudget = () => changeView('EDIT_BUDGET');
-
-export const chooseEditExpenditure = () => changeView('EDIT_EXPENDITURE');
