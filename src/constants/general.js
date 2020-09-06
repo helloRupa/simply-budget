001e80bde7da5aa998954cf9a64721c04362ddf5
Rupa Dhillon <rupa211984@gmail.com>
2020-09-05 18:39:40 -0700

    Replace msPerDay math w/ static value

--- src/constants/general.js
+++ src/constants/general.js
@@ -9,11 +9,7 @@ export const periodDisplayOptions = {
   week: 'Week'
 };
 
-const dayHours = 24;
-const hourMins = 60;
-const minSecs = 60;
-const secMs = 1000;
-export const msPerDay = dayHours * hourMins * minSecs * secMs;
+export const msPerDay = 86400000;
 
 export const weekDays = 7;
 export const monthDays = 30;
