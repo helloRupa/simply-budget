54c286cb67ef203f696066886fd45ceda914e04a
Rupa Dhillon <rupa211984@gmail.com>
2020-09-18 23:04:43 -0700

    Update click or hold to make modal appear without release

--- src/shared/ClickOrHold.js
+++ src/shared/ClickOrHold.js
@@ -1,27 +1,24 @@
 import React, { useState } from 'react';
 
 function ClickOrHold({ holdCallback, clickCallback, children }) {
-  const [timeDown, setTimeDown] = useState(0);
-  const [pressState, setPressState] = useState('');
+  const [timer, setTimer] = useState('');
 
-  const startTime = () => setTimeDown(Date.now());
-  const setClickType = () => {
-    Date.now() - timeDown < 500 ? setPressState('click') : setPressState('hold');
+  const startHold = e => {
+    setTimer(setTimeout(() => {
+      if (typeof holdCallback === 'function') {
+        holdCallback(e);
+      }
+    }, 500));
   };
 
   const handleClick = e => {
-    if (pressState === 'hold' && typeof holdCallback === 'function') {
-      holdCallback(e);
-    }
-
-    if (pressState === 'click' && typeof clickCallback === 'function') {
+    if (typeof clickCallback === 'function') {
+      clearTimeout(timer);
       clickCallback(e);
     }
-
-    setPressState('');
   };
 
-  return <span onMouseDown={startTime} onMouseUp={setClickType} onClick={handleClick}>
+  return <span onMouseDown={startHold} onClick={handleClick}>
     {children}
   </span>
 }
