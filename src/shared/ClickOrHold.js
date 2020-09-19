3ddd86414dadc041e3b4c97079467a91e010fa08
Rupa Dhillon <rupa211984@gmail.com>
2020-09-19 15:46:50 -0700

    Prevent click from happening after hold

--- src/shared/ClickOrHold.js
+++ src/shared/ClickOrHold.js
@@ -2,23 +2,27 @@ import React, { useState } from 'react';
 
 function ClickOrHold({ holdCallback, clickCallback, children }) {
   const [timer, setTimer] = useState('');
+  const [wasHeld, setWasHeld] = useState(false);
 
   const startHold = e => {
+    setWasHeld(false);
+
     setTimer(setTimeout(() => {
       if (typeof holdCallback === 'function') {
         holdCallback(e);
+        setWasHeld(true);
       }
     }, 500));
   };
 
   const handleClick = e => {
-    if (typeof clickCallback === 'function') {
+    if (!wasHeld && typeof clickCallback === 'function') {
       clearTimeout(timer);
       clickCallback(e);
     }
   };
 
-  return <span onMouseDown={startHold} onClick={handleClick}>
+  return <span onMouseDown={startHold} onClick={handleClick} onTouchStart={startHold} onTouchEnd={handleClick}>
     {children}
   </span>
 }
