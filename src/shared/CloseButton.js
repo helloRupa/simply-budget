57e6b3d7796c1009ab9d269ef74abc2da0d95cde
Rupa Dhillon <rupa211984@gmail.com>
2020-09-20 23:46:10 -0700

    Update close button

--- src/shared/CloseButton.js
+++ src/shared/CloseButton.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import Button from './Button';
+import close from '../images/close.svg';
 
 function CloseButton({ callback }) {
-  return <Button callback={callback} className="close" display="X" />
+  return <Button 
+    callback={callback} 
+    className="close" 
+    display={<img src={close} alt="close" />} />
 }
 
 export default CloseButton;
