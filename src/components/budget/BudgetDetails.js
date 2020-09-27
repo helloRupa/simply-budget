2b691a200e9ad104f3a12e3af06cdc035f2483ae
Rupa Dhillon <rupa211984@gmail.com>
2020-09-27 16:45:24 -0700

    Animate tracking number change

--- src/components/budget/BudgetDetails.js
+++ src/components/budget/BudgetDetails.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { calculateTracking, formattedSingleBudgetTracking } from '../../utils/calculate';
 import { setTrackingClassName } from '../../utils/classNameSelectors';
 import { displayDate, formatNumber } from '../../utils/format';
@@ -9,6 +9,17 @@ function BudgetDetails({
   budget: { currency, frequency, limit, startDate } 
 }) {
   const trackingClassName = setTrackingClassName(calculateTracking({ expenditures, budget }));
+  const [showTotalTracking, setShowTotalTracking] = useState('show-total-tracking');
+  const firstLoad = useRef(true);
+
+  useEffect(() => {
+    if (!firstLoad.current) {
+      setShowTotalTracking('');
+      setTimeout(() => setShowTotalTracking('show-total-tracking'), 500);
+    }
+
+    firstLoad.current = false;
+  }, [expenditures]);
 
   return <div className="budget-details">
   <span className="goal">
@@ -19,7 +30,7 @@ function BudgetDetails({
 
   <span className="total-tracking">
     <span>Total Saved</span>
-    <span className={trackingClassName}>
+    <span className={`${trackingClassName} ${showTotalTracking}`}>
       {currency}{formattedSingleBudgetTracking(expenditures, budget)}
     </span>
     <span>since { displayDate(startDate) }</span>
