06439c07904039ad459e4e529cc59b5167fb099e
Rupa Dhillon <rupa211984@gmail.com>
2020-09-26 18:54:04 -0700

    Move tooltip into view

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { clearTooltip } from '../actions/tooltip_actions';
 
 function Tooltip({ tooltip, clearTooltip }) {
+  const [moveTooltip, setMoveTooltip] = useState('');
+
   if (tooltip) {
-    setTimeout(() => clearTooltip(), 3000);
+    setTimeout(() => {
+      setMoveTooltip(() => '');
+      clearTooltip();
+    }, 3000);
+
+    setTimeout(() => setMoveTooltip(() => 'move-tooltip'), 5);
 
     return (
-      <div className="tooltip">
+      <div className={`tooltip ${moveTooltip}`}>
         <div>{ tooltip }</div>  
         <span className="triangle"></span>
       </div>
