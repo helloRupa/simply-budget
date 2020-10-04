e0b904a8a8471c33203fbefdeab36175dab6cfac
Rupa Dhillon <rupa211984@gmail.com>
2020-10-03 19:14:43 -0700

    Fix tooltip id

--- src/reducers/tooltip_reducer.js
+++ src/reducers/tooltip_reducer.js
@@ -1,11 +1,12 @@
 import { SET_TOOLTIP, CLEAR_TOOLTIP } from '../constants/redux';
 
 const tooltipState = [];
+let id = 1;
 
 function tooltipReducer(state = tooltipState, action) {
   switch(action.type) {
     case SET_TOOLTIP:
-      return [...state, { tooltip: action.tooltip, id: state.length }];
+      return [...state, { tooltip: action.tooltip, id: ++id }];
     case CLEAR_TOOLTIP:
       return state.filter(tt => tt.id !== action.id);
     default:
