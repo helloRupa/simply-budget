f80da73371642c8bf512c1a9c98f40caae181f61
Rupa Dhillon <rupa211984@gmail.com>
2021-01-01 22:32:28 -0800

    Test calcs with static data

--- src/tests/calculations.test.js
+++ src/tests/calculations.test.js
@@ -1,7 +1,11 @@
 import * as calc from "../utils/calculate";
 import * as factory from "./factories/budgets_expenses";
-import * as validators from "./factories/validators";
 import { roundedDecimal } from "./factories/normalizers";
+import * as staticData from "./factories/static_data";
+
+const budgetsExpenses = staticData.budgetsExpenses;
+const { budgets, expenses } = budgetsExpenses;
+const trackingPerBudget = staticData.trackingPerBudget;
 
 describe("calculations", () => {
   describe("totalSpent", () => {
@@ -10,53 +14,26 @@ describe("calculations", () => {
     });
 
     it("sums the correct amount for a chosen budget", () => {
-      const budgetId = 3;
-      const budgetsExpenses = factory.generateBudgetsAndExpenses(
-        budgetId,
-        "2020/12/25",
-        10,
-        0
-      );
-      const total = validators.totalSpent(budgetsExpenses, budgetId);
-
-      expect(calc.totalSpent(budgetsExpenses.expenses, budgetId)).toBe(total);
+      budgets.forEach((budget) => {
+        expect(roundedDecimal(calc.totalSpent(expenses, budget.id))).toBe(
+          roundedDecimal(staticData.totalSpentPerBudget[budget.id])
+        );
+      });
     });
   });
 
   describe("calculateTracking", () => {
-    const budgetsExpenses = factory.generateBudgetsAndExpenses(
-      1,
-      Date.now(),
-      7,
-      factory.randomAmount(200, 27)
-    );
-    const { budgets, expenses } = budgetsExpenses;
-
     it("returns the amount left to spend based on the current period, truncated value, and amount spent", () => {
-      const budget = budgets[0];
-      const testTotal = calc.calculateTracking({
-        expenditures: expenses,
-        budget,
+      budgets.forEach((budget) => {
+        const testTotal = calc.calculateTracking({
+          expenditures: expenses,
+          budget,
+        });
+
+        expect(roundedDecimal(trackingPerBudget[budget.id])).toBe(
+          roundedDecimal(testTotal)
+        );
       });
-      const total = validators.budgetTracking(budgetsExpenses, budget.id);
-
-      expect(roundedDecimal(testTotal)).toBe(roundedDecimal(total));
-    });
-
-    it("can handle overspending", () => {
-      const budget = budgets[0];
-
-      expenses[budget.id].push({
-        amount: budget.limit * budget.currentPeriod + 100,
-      });
-
-      const total = validators.budgetTracking(budgetsExpenses, budget.id);
-      const testTotal = calc.calculateTracking({
-        expenditures: expenses,
-        budget,
-      });
-
-      expect(roundedDecimal(testTotal)).toBe(roundedDecimal(total));
     });
 
     it("returns 0 for a budget that hasn't started", () => {
@@ -70,18 +47,12 @@ describe("calculations", () => {
 
   describe("calculateTotalTracking", () => {
     it("returns the total amount left to spend across all budgets", () => {
-      const budgetsExpenses = factory.generateBudgetsAndExpenses(
-        1,
-        Date.now(),
-        7,
-        factory.randomAmount(200, 27)
-      );
       const { budgets, expenses } = budgetsExpenses;
-
       const testTotal = calc.calculateTotalTracking(budgets, expenses);
-      const total = validators.allBudgetsTracking(budgetsExpenses);
 
-      expect(roundedDecimal(testTotal)).toBe(roundedDecimal(total));
+      expect(roundedDecimal(testTotal)).toBe(
+        roundedDecimal(staticData.totalTrackingAllBudgets)
+      );
     });
 
     it("returns 0 when there are no budgets", () => {
